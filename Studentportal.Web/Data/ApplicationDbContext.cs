a00570bf8fce065bf78a30312b3d302adf5af4f2
dgyrsenevirathne <yasithsenevirathna01@gmail.com>
2024-08-13 12:14:04 +0530

    Initial commit message

---
+++ Studentportal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Studentportal.Web.Models.Entities;
+
+namespace Studentportal.Web.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {   
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Studentportal.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Studentportal.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
